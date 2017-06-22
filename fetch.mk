4a39be90fa1fb199266907e8afff552d29d6315d
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-06-22 15:35:33 -0700

    unanimity needs submodules

--- fetch.mk
+++ fetch.mk
@@ -1,7 +1,10 @@
+SHELL=bash -e
 GIT_CLONE=git clone --depth=1
 
 all: repos/pypeFLOW repos/FALCON repos/FALCON-polish repos/FALCON-pbsmrtpipe repos/GenomicConsensus repos/pbcommand repos/pbcore repos/pbcoretools repos/pbalign repos/ConsensusCore repos/unanimity
 repos/%:
-	cd repos; ${GIT_CLONE} ssh://git@bitbucket.nanofluidics.com:7999/sat/$*
+	cd repos && ${GIT_CLONE} ssh://git@bitbucket.nanofluidics.com:7999/sat/$*
 repos/pbcommand:
-	cd repos; ${GIT_CLONE} ssh://git@bitbucket.nanofluidics.com:7999/sl/pbcommand
+	cd repos && ${GIT_CLONE} ssh://git@bitbucket.nanofluidics.com:7999/sl/pbcommand
+repos/unanimity:
+	cd repos && ${GIT_CLONE} --recursive ssh://git@bitbucket.nanofluidics.com:7999/sl/pbcommand

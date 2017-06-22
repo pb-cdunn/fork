7ef8f78d7a736b89f1ebce5437d362969e70fc95
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-06-22 07:55:32 -0700

    More modules might help?

--- env.sh
+++ env.sh
@@ -3,11 +3,14 @@ module purge
 module load git
 module load python
 module load gcc/4.9.2
-#module load ccache
 module load cmake
-#module load boost
+
+module load ccache
+module load boost
+module load hdf5-tools/1.8.16
+module load openblas # so ConsensusCore can use numpy
+
 #module load zlib # IMPORTANT: Do NOT load this!
-#module load hdf5-tools/1.8.16
 
 set -vex
 

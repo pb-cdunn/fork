c85cfdfd398a87a6dd59fe991bfc454176456765
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-07-26 06:35:25 -0700

    python/2.7.13-UCS2

--- env-pb.sh
+++ env-pb.sh
@@ -1,7 +1,7 @@
 source /mnt/software/Modules/current/init/bash
 module purge
 module load git
-module load python/2.7.13-UCS4
+module load python/2.7.13-UCS2
 module load gcc/4.9.2
 module load cmake
 module load swig

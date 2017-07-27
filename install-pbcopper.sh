3aeab0effad50354c958f6d1125d88defa593118
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-07-27 14:27:06 -0700

    Clarify how libpbcopper.so could be built

--- install-pbcopper.sh
+++ install-pbcopper.sh
@@ -6,12 +6,14 @@ mkdir -p _build
 cd _build
 
 cmake \
+          -Dpbcopper_build_shared=OFF \
           -Dpbcopper_build_tests=OFF \
           -DBoost_INCLUDE_DIRS=${PREFIX}/include \
     ..
-
 make
+
 rsync -av lib/libpbcopper.a ${PREFIX}/lib/
+#rsync -av lib/libpbcopper.a lib/libpbcopper.so ${PREFIX}/lib/
 #rsync -av ../include/pbcopper ${PREFIX}/include/
 cd ..
 ln -sf $(pwd)/include/pbcopper ${PREFIX}/include/

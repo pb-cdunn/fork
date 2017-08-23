4c5ed93fbbc0f4ad58d930e3b221084307f7798f
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-08-23 06:42:40 -0700

    PB env from pbbam/bamboo_build_meson.sh

---
+++ env-pb1.sh
@@ -0,0 +1,27 @@
+type module >& /dev/null || . /mnt/software/Modules/current/init/bash
+################
+# DEPENDENCIES #
+################
+
+# buildsystem dependencies
+module load meson/0.41.1
+module load ninja/1.7.2
+
+module load gcc/6.4.0
+module load ccache/3.3.4
+module load git/2.8.3
+
+module load zlib/1.2.11
+module load htslib/1.5
+module load gtest/1.8.0_p20170819
+module load boost/1.60
+module load cram/0.7
+
+# remove trailing "/include", because CMake is brain-damaged
+BOOST_ROOT=${BOOST_ROOT%/include}
+# unset these variables to have meson discover all
+# boost-dependent variables from BOOST_ROOT alone
+unset BOOST_INCLUDEDIR
+unset BOOST_LIBRARYDIR
+
+#[[ $USER == "bamboo" ]] && export CCACHE_DIR=/mnt/secondary/Share/tmp/bamboo.mobs.ccachedir || true

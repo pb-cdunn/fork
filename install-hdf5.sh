c6267a51aebfb6320f41170bab3e6617b0b71463
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-08-23 07:23:34 -0700

    hdf5-tools needs C++

--- install-hdf5.sh
+++ install-hdf5.sh
@@ -2,7 +2,7 @@
 
 set -vex
 
-HDF_ROOT=/mnt/software/h/hdf5-tools/1.8.16/centos-6
+HDF_ROOT=/mnt/software/h/hdf5-tools/1.8.19
 rsync -av ${HDF_ROOT}/include/ ${PREFIX}/include/
 rsync -av ${HDF_ROOT}/lib/lib*.so* ${PREFIX}/lib/
 

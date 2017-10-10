7252b619b0b907bf9e1649950b76aae169f8ff9a
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-10-10 09:03:38 -0700

    Fix gh clones

--- fetch.mk
+++ fetch.mk
@@ -6,14 +6,20 @@ SL=repos/pbcommand
 
 all: repos/nim-falcon repos/seqan repos/bam2fastx repos/FALCON_unzip repos/minimap repos/cDNA_Cupcake ${SAT} ${SL}
 
-repos/nim-falcon: BRANCH=dev BASE=git@github.com:bio-nim/nim-falcon
-	${GIT_CLONE} -b ${BRANCH} ${BASE}/$* && mv $* repos/
-	cd $@; git submodule update --init
+repos/nim-falcon:   BRANCH=dev
+repos/seqan:        BRANCH=master
+repos/cDNA_Cupcake: BRANCH=master
+repos/minimap:      BRANCH=master
+
+repos/cDNA_Cupcake: BASE=git@github.com:/Magdoll
+repos/minimap:      BASE=https://github.com/lh3
+repos/nim-falcon:   BASE=git@github.com:bio-nim
+
+nim-falcon-sub: repos/nim-falcon
+	cd repos/nim-falcon; git submodule update --init
 
 BASE=git@github.com:PacificBiosciences
 BRANCH=develop
-repos/cDNA_Cupcake: BRANCH=master BASE=git@github.com:/Magdoll
-repos/minimap:      BRANCH=master BASE=https://github.com/lh3
 #${SAT}: BASE=ssh://git@bitbucket.nanofluidics.com:7999/SAT
 #${SL}:  BASE=ssh://git@bitbucket.nanofluidics.com:7999/SL
 repos/%:

01acf94f070775dd3dabf88c00b047a067ad2f45
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-10-16 07:06:46 -0700

    I guess bam2fastax calls HTSLIB_INCLUDE_DIRS redundant

--- install-bam2fastx.sh
+++ install-bam2fastx.sh
@@ -14,7 +14,6 @@ cmake \
               -DZLIB_INCLUDE_DIRS=${PREFIX}/include \
               -DZLIB_LIBRARIES=${PREFIX}/lib/libz.${DYLIB} \
               -DPacBioBAM_LIBRARIES=${PREFIX}/lib/libpbbam.${DYLIB} \
-              -DHTSLIB_INCLUDE_DIRS=${PREFIX}/include \
               -DHTSLIB_LIBRARIES=${PREFIX}/lib/libhts.a \
               -Dpbcopper_INCLUDE_DIRS=${PREFIX}/include \
               -Dpbcopper_LIBRARIES=${PREFIX}/lib/libpbcopper.a \

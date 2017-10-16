8e758dfceae6d57c86667092242c8258775b946e
Christopher Dunn <cdunn@pacificbiosciences.com>
2017-10-16 06:56:16 -0700

    Keep symlinks for mummer tools

--- extra.mk
+++ extra.mk
@@ -1,6 +1,6 @@
 all: done/extra-install
 
 done/extra-install:
-	rsync -av $$(which nucmer) ${PREFIX}/bin/
-	rsync -av $$(which show-coords) ${PREFIX}/bin/
+	rsync -avL $$(which nucmer) ${PREFIX}/bin/
+	rsync -avL $$(which show-coords) ${PREFIX}/bin/
 	touch $@

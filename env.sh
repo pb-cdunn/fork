5a1d61aa23f2a4165f7f2deef29e47add2f5da12
Christopher Dunn <cdunn@pacificbiosciences.com>
2018-02-16 12:51:22 -0800

    Try DEBUG=1

--- env.sh
+++ env.sh
@@ -6,3 +6,6 @@ PATH=${PREFIX}/bin:${PATH}:/usr/local/mummer
 LD_LIBRARY_PATH=${PREFIX}/lib:${LD_LIBRARY_PATH}
 DYLD_LIBRARY_PATH=${PREFIX}/lib:${DYLD_LIBRARY_PATH}:/usr/local/lib
 export PFHOME PREFIX PYTHONUSERBASE PATH LD_LIBRARY_PATH DYLD_LIBRARY_PATH
+
+# not sure, but this might affect cmake
+export DEBUG=1

7de2db8de1d5a9b94831714b85bcfbe213f7b85e
Christopher Dunn <cdunn@pacificbiosciences.com>
2018-02-16 12:52:57 -0800

    Fix CC1 install (BOOST_ROOT)

--- install-ConsensusCore.sh
+++ install-ConsensusCore.sh
@@ -1,5 +1,10 @@
 set -vex
+set -o pipefail
 
 #pip install -v --user --no-deps --install-option="--swig=$(PREFIX)/bin/swig" --install-option="--swig-lib=$(PREFIX)/share/swig/3.0.8" --install-option="--boost=$(BOOST_ROOT)/include" .
 
-python setup.py -v install --user --boost=${PREFIX}/include
+#python setup.py -v install --user --boost=${PREFIX}/include
+
+python setup.py bdist_wheel --boost=${BOOST_ROOT}
+
+echo dist/ConsensusCore-*.whl | xargs pip install --user --verbose
